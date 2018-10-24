4d3853d01fa221edba6f5b6e6f0ed91e0dd4161d
Duli Chand Prajapati <duli.prajapati@infinitylabs.in>
2018-10-24 18:05:52 +0530

    Routing Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
 
-    RouterModule.forRoot(routes) 
+    RouterModule.forRoot(routes,{useHash: true}) 
   ],
   exports: [ RouterModule ],
   declarations: []
