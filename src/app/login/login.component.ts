68d982866b818f209f3e4612aa3aec5efdb71ea4
Duli Chand Prajapati <duli.prajapati@infinitylabs.in>
2018-10-26 16:29:02 +0530

    add otp form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -7,23 +7,37 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
   loginService: any;
 
   constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   LoginForm: FormGroup;
+  OtpForm: FormGroup;
+
   showErrors = false;
   submitted = false;
+  optBox = false;
 
   ngOnInit() {
-    this.LoginForm = new FormGroup({
+    this.LoginForm = this.formBuilder.group({
       'mobileNo': new FormControl(null, { validators: [Validators.required] })
-    });
+      // quantity: this.fb.control(123)
+    })
+
+    // this.LoginForm = new FormGroup({
+    //   'mobileNo': new FormControl(null, { validators: [Validators.required] })
+    // });
     sessionStorage.clear();
   }
+  ngAfterViewInit(){
+    this.OtpForm = this.formBuilder.group({
+      'optNumber': new FormControl(null, { validators: [Validators.required] })
+      // quantity: this.fb.control(123)
+    })
+  }
 
-  loginSubmit() {
+  loginSubmit() { // valid login function 
     this.submitted = true;
     if (this.LoginForm.valid) {
       this.verifyLogin();
@@ -33,8 +47,40 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  verifyLogin() {
+
+  verifyLogin() { //verifying the user's mobile no
+    this.optBox = true;
     this.loginService.loginApi(this.LoginForm.value)
+      .subscribe(
+        (exp_res) => {
+          if (exp_res.status === 'success') {
+            sessionStorage.setItem('userData', JSON.stringify(exp_res.data.user));
+            sessionStorage.setItem('token', JSON.stringify(exp_res.data.token));
+            this.optBox = true;
+          } else {
+            this.showErrors = true;
+            this.submitted = false;
+          }
+        },
+        (error: Error) => {
+          this.showErrors = true;
+          this.submitted = false;
+        }
+      );
+  }
+
+  otpSubmit() { // valid otp number function 
+    console.log('hiui');
+    this.submitted = true;
+    if (this.OtpForm.valid) {
+      // this.OtpLogin();
+    } else {
+      this.showErrors = true;
+      console.log('%c value is not filled', 'color:red');
+    }
+  }
+  OtpLogin(){
+    this.loginService.otpApi(this.OtpForm.value)
       .subscribe(
         (exp_res) => {
           if (exp_res.status === 'success') {
